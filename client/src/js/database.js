09a1713fb659da0b334d43d748230ef9132e4f79
Warren Haskins <warrenhaskins1@gmail.com>
2022-02-13 14:21:15 -0500

    updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,44 +1,66 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+/////////////////////////////////////////////////////////////////////////////////
+//Get All
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
+export const getDb = async (value) => {
   console.log("GET from the Database");
 
   //Create a connection to the database and include the version that we want to use
-  const contactDb = await openDB("contact", 1);
+  const jateDb = await openDB("jate", 1);
 
   //Create a transaction with the database and data priviledges i.e "readwrite/readonly" use tx for transaction.
-  const tx = contactDb.transaction("contact", "readwrite");
+  const tx = jateDb.transaction("jate", "readwrite");
 
-  //Open the objet store, i.e. where the data will be stored for the offline
-  const store = tx.objectStore("contact");
+  //Open the object store, i.e. where the data will be stored for the offline
+  const store = tx.objectStore("jate");
 
-  //Use the .add() Method on the store to pass in the content
-  //***************** */Determine what we are going to store*****************************
-  const request = store.add({name: name, email: email});
+  //Get all the data from the database
+  const request = store.getAll();
 
   //Get confirmation of the request
   const result = await request;
   console.log("Data has been saved to the database", result);
 
-  //Log error if one occurs 
-  console.error('getDb not implemented');
-}
+  //Log error if one occurs
+  console.error("getDb not implemented");
+};
 
+///////////////////////////////////////////////////////////////////////////////////////
+//Put Update
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (value, id) => {
+  console.log("PUT/update to the database");
+
+  //Create a connection to the database and include the version that we want to use
+  const jateDb = await openDB("jate", 1);
+
+  //Create a transaction with the database and data priviledges i.e "readwrite/readonly" use tx for transaction.
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  //Open the object store, i.e. where the data will be stored for the offline
+  const store = tx.objectStore("jate");
+
+  //Use the add.() to pass the content
+  const request = store.add({ value: value, id: id });
+
+  //Get confirmation of the request
+  const result = await request;
+  console.log("Data has been UPDATED to the database", result);
+
+  console.error("putDb not implemented");
+};
 
 initdb();
